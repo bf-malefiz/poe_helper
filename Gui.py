07e559386d4a3f74889fefdd6bc0f9de7502b39f
PhilippDahlke <Philipp_dahlke@gmx.de>
2021-02-27 13:52:00 +0100

    gui enhance

--- Gui.py
+++ Gui.py
@@ -4,7 +4,7 @@ from itertools import islice
 from model import *
 from pynput.mouse import Listener
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QComboBox, QLabel,QApplication, QWidget, QAction, QLineEdit, QPushButton, QListWidget, QCheckBox
+from PyQt5.QtWidgets import QPushButton,QComboBox, QLabel,QApplication, QWidget, QAction, QLineEdit, QPushButton, QListWidget, QCheckBox
 
 
 class App(QWidget):
@@ -15,8 +15,8 @@ class App(QWidget):
         self.title = "PoE-Helper"
         self.left = 100
         self.top = 100
-        self.width = 450
-        self.height = 700
+        self.width = 320
+        self.height = 680
         self.listener = Listener(on_click=Model.on_click)
         self.model = Model()
         self.initUI()
@@ -43,10 +43,10 @@ class App(QWidget):
         
         #Textbox_Depth
         self.comb_depth = QComboBox(self)
-        self.comb_depth.addItems(["1","2","3","4","5","6","7"])
+        self.comb_depth.addItems(["1 - None","2 - cluster-, jewels, maps, watchstones","3 - heist-util, abyss-jewels","4 - without implicit","5 - with implicit","6 - none"])
         self.comb_depth.currentIndexChanged.connect(self.selectionchange)
         self.comb_depth.move(20,100)
-        self.comb_depth.resize(60,20)
+        self.comb_depth.resize(280,20)
 
         #Label_Mod
         self.label_mod = QLabel(self)
@@ -72,41 +72,53 @@ class App(QWidget):
         self.button = QPushButton('Save Mod', self)
         self.button.move(20,550)
         self.button.resize(70,50)
+        self.button.setStyleSheet("background-color : rgba(0, 255, 0, 50%)") 
         self.button.clicked.connect(self.on_click_save)
 
         #Delete Button
         self.button_del = QPushButton('Delete Mod', self)
-        self.button_del.move(160,550)
+        self.button_del.move(230,550)
         self.button_del.resize(70,50)
+        self.button_del.setStyleSheet("background-color : rgba(255, 0, 0, 50%)") 
         self.button_del.clicked.connect(self.on_click_del)
 
-        self.textbox_min.setText("1")
+        self.textbox_min.setText("50")
         self.textbox_mod.setText("to maximum Life")
 
-        #Checkbox
-        self.checkbox = QCheckBox("Activate Listener",self)
-        self.checkbox.setChecked(False)
-        self.checkbox.stateChanged.connect(self.clickBox)
-        self.checkbox.move(20,620)
+        #Pushbutton
+        self.pushbutton = QPushButton("Activate Listener",self)
+        self.pushbutton.setGeometry(200, 150, 100, 40)
+        self.pushbutton.setCheckable(True)
+        self.pushbutton.clicked.connect(self.changeState)
+        self.pushbutton.setStyleSheet("background-color : lightgrey") 
+        self.pushbutton.move(110,620)
 
+        self.update()
         self.show()
+    
+    def changeState(self):
 
-    def clickBox(self, state):
-        
-        if state == QtCore.Qt.Checked:
+        if self.pushbutton.isChecked():
+            self.pushbutton.setStyleSheet("background-color : lightblue") 
             self.listener = Listener(on_click=Model.on_click)
             Model.poe_listener(True, self.listener)
         else:
+            self.pushbutton.setStyleSheet("background-color : lightgrey")
             Model.poe_listener(False, self.listener)
-    
+
+
     def selectionchange(self,i):
         model.item_depth = i + 1
     
     def on_click_del(self):
+
         row = self.modlist.currentRow()
-        self.modlist.takeItem(row)
-        del model.requirements[next(islice(model.requirements,row,None))]
-        print(model.requirements)
+        print(row)
+        if row != -1:
+            self.modlist.takeItem(row)
+            del model.requirements[next(islice(model.requirements,row,None))]
+        else:
+            pass
 
     def on_click_save(self):
         if self.textbox_mod.text():
