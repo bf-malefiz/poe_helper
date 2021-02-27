70b5be3edb9133a5098b4ddf54cc297a21bc2239
PhilippDahlke <Philipp_dahlke@gmx.de>
2021-02-27 14:34:32 +0100

    add struct

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-import Gui, sys
-    
+import Gui, sys, model, item_data
+
 def main():
     app = Gui.QApplication(sys.argv)
     ex = Gui.App()
     ex.show()
 
     sys.exit(app.exec_())
-	
+
 if __name__ == '__main__':
     main()
