fdeee935df2a01d7fdd65ed88ba22d7262cf18dd
PhilippDahlke <Philipp_dahlke@gmx.de>
2021-03-04 17:44:42 +0100

    removed depth, adjusted gui

--- src/Gui.py
+++ src/Gui.py
@@ -16,7 +16,7 @@ class App(QWidget):
         self.left = 100
         self.top = 100
         self.width = 320
-        self.height = 680
+        self.height = 410
         self.listener = Listener(on_click=Model.on_click)
         self.model = Model()
         self.initUI()
@@ -29,55 +29,43 @@ class App(QWidget):
         #Label_Min
         self.label_min = QLabel(self)
         self.label_min.setText("Minimum")
-        self.label_min.move(20,20)    
+        self.label_min.move(20,20)  
 
         #Textbox_Min
         self.textbox_min = QLineEdit(self)
-        self.textbox_min.move(20,40)
+        self.textbox_min.move(100,15)
         self.textbox_min.resize(60,20)
-
-        #Label_Depth
-        self.label_depth = QLabel(self)
-        self.label_depth.setText("Depth")
-        self.label_depth.move(20,80)    
-        
-        #Textbox_Depth
-        self.comb_depth = QComboBox(self)
-        self.comb_depth.addItems(["1 - None","2 - cluster-, jewels, maps, watchstones","3 - heist-util, abyss-jewels","4 - without implicit","5 - with implicit","6 - none"])
-        self.comb_depth.currentIndexChanged.connect(self.selectionchange)
-        self.comb_depth.move(20,100)
-        self.comb_depth.resize(280,20)
 
         #Label_Mod
         self.label_mod = QLabel(self)
         self.label_mod.setText("Desired Mod")
-        self.label_mod.move(20,140)    
+        self.label_mod.move(20,50)    
 
         #Textbox_Mod
         self.textbox_mod = QLineEdit(self)
-        self.textbox_mod.move(20,160)
+        self.textbox_mod.move(20,70)
         self.textbox_mod.resize(280,20)
 
         #Label_Modlist
         self.label_modlist = QLabel(self)
         self.label_modlist.setText("Modlist")
-        self.label_modlist.move(20,200)  
+        self.label_modlist.move(20,110)  
 
         #Textbox_Modlist
         self.modlist = QListWidget(self)
-        self.modlist.move(20,220)
-        self.modlist.resize(280,300)
+        self.modlist.move(20,130)
+        self.modlist.resize(280,150)
 
         #Save Button
         self.button = QPushButton('Save Mod', self)
-        self.button.move(20,550)
+        self.button.move(20,300)
         self.button.resize(70,50)
         self.button.setStyleSheet("background-color : rgba(0, 255, 0, 50%)") 
         self.button.clicked.connect(self.on_click_save)
 
         #Delete Button
         self.button_del = QPushButton('Delete Mod', self)
-        self.button_del.move(230,550)
+        self.button_del.move(230,300)
         self.button_del.resize(70,50)
         self.button_del.setStyleSheet("background-color : rgba(255, 0, 0, 50%)") 
         self.button_del.clicked.connect(self.on_click_del)
@@ -85,13 +73,13 @@ class App(QWidget):
         self.textbox_min.setText("50")
         self.textbox_mod.setText("to maximum Life")
 
-        #Pushbutton
+        #Listener Pushbutton
         self.pushbutton = QPushButton("Activate Listener",self)
         self.pushbutton.setGeometry(200, 150, 100, 40)
         self.pushbutton.setCheckable(True)
         self.pushbutton.clicked.connect(self.changeState)
         self.pushbutton.setStyleSheet("background-color : lightgrey") 
-        self.pushbutton.move(110,620)
+        self.pushbutton.move(110,350)
 
         self.update()
         self.show()
@@ -121,12 +109,20 @@ class App(QWidget):
             pass
 
     def on_click_save(self):
-        if self.textbox_mod.text():
+        if self.textbox_mod.text() and self.textbox_min.text():
             textbox_value_mod = self.textbox_mod.text()
             minimum = {"minimum": float(self.textbox_min.text())}
 
             self.modlist.addItem(str(minimum["minimum"]) +" : " + textbox_value_mod)
             model.requirements[textbox_value_mod] = minimum
+            self.textbox_min.setText("")
+            self.textbox_mod.setText("")
+
+        elif self.textbox_mod.text() and not self.textbox_min.text():
+            textbox_value_mod = self.textbox_mod.text()
+
+            self.modlist.addItem(textbox_value_mod + " (raw)")
+            model.requirements[textbox_value_mod] = None
             self.textbox_min.setText("")
             self.textbox_mod.setText("")
         else:
