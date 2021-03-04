42ddd17ec33c3cd41d600f39837f05051d327836
PhilippDahlke <Philipp_dahlke@gmx.de>
2021-03-04 17:46:53 +0100

    removed depth, checks whole clipboard know

--- src/item_data.py
+++ src/item_data.py
@@ -1,29 +1,36 @@
 import re
 import pyperclip
+from operator import add
+from functools import reduce
 
 class item_data():
 
-    def __init__(self, item_depth=1):
+    def __init__(self):
         super().__init__()
-        self.item_depth=item_depth
         self.values = []
         self.mods = []
         self.clipboard = pyperclip.paste()
 
     def process_item_data(self):
-        s2 = self.clipboard.split("--------")[self.item_depth]
-        s3 = s2.split("\r\n")[1:-1]
+        s2 = self.clipboard.split("--------")
+        s3 = [s.split("\r\n")[1:-1] for s in s2[:-1]]
+        s4 = None
+        try:
+            s4 = reduce(add, s3)
+            for i in s4:
+                tmp = [s.replace('%','') for s in i.split()]
+                tmp_values = []
+                tmp_mods = []
+                for s in tmp:
+                    r_values = re.findall(r"[-+]?\d*\.\d+|\d+", s)
+                    if r_values:
+                        tmp_values.append(float(r_values[0]))
+                    r_mods = re.findall(r"\b[^\d\W]+\b", s)
+                    if r_mods:
+                        tmp_mods.append(r_mods[0])
+                self.values.append(tmp_values)
+                self.mods.append(tmp_mods)
 
-        for i in s3:
-            tmp = [s.replace('%','') for s in i.split()]
-            tmp_values = []
-            tmp_mods = []
-            for s in tmp:
-                r_values = re.findall(r"[-+]?\d*\.\d+|\d+", s)
-                if r_values:
-                    tmp_values.append(float(r_values[0]))
-                r_mods = re.findall(r"\b[^\d\W]+\b", s)
-                if r_mods:
-                    tmp_mods.append(r_mods[0])
-            self.values.append(tmp_values)
-            self.mods.append(tmp_mods)
+
+        except TypeError:
+            pass
